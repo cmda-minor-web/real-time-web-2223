8b0fa01cbf48ad226aa57f3d93009446b0d3a703
SundousKanaan <94317411+SundousKanaan@users.noreply.github.com>
2023-04-17 23:07:43 +0200

    test 3

--- public/script/script.js
+++ public/script/script.js
@@ -1,3 +1,57 @@
+// const messages = document.querySelector('section ul');
+// const input = document.querySelector('#message-input');
+// const sendMessage = document.querySelector('#message-button');
+// const usernameInput = document.querySelector('#username-input');
+// const loggin= document.querySelector('main section:first-of-type')
+// const chatScreen= document.querySelector('main section:last-of-type')
+// const logginButton = document.querySelector('main section:first-of-type > button')
+
+// chatScreen.classList.add("hidden");
+
+// logginButton.addEventListener('click' , () => {
+//     loggin.classList.add("hidden");
+//     chatScreen.classList.remove("hidden");
+
+// })
+
+
+// input.addEventListener('input', () => {
+//     const inputValue = input.value;
+//     // Doe hier iets met de waarde van het invoerveld
+//     console.log(inputValue);
+//     chatScreen.classList.add('focus')
+// });
+
+
+// sendMessage.addEventListener('click', (event) => {
+//     chatScreen.classList.remove('focus')
+
+//     event.preventDefault();
+//     if (input.value) {
+
+//         const chat ={
+//             username: usernameInput.value,
+//             message: input.value
+//         }
+
+//         socket.emit('chat message', chat);
+//         input.value = '';
+//       }
+// });
+
+// socket.on('chat message', (msg) => {
+//     console.log('chat message: ', msg.message);
+//     console.log(chatScreen);
+
+//     const element = document.createElement('li');
+//     element.textContent = ` ${msg.username}: ${msg.message} `;
+
+//     messages.appendChild(element);
+//     messages.scrollTop = messages.scrollHeight;
+// });
+
+
+
 const messages = document.querySelector('section ul');
 const input = document.querySelector('#message-input');
 const sendMessage = document.querySelector('#message-button');
@@ -12,23 +66,21 @@ logginButton.addEventListener('click' , () => {
     loggin.classList.add("hidden");
     chatScreen.classList.remove("hidden");
     socket.emit('focus', true); // Verzend de focus class naar andere clients
-})
+});
 
 
 input.addEventListener('input', () => {
     const inputValue = input.value;
     // Doe hier iets met de waarde van het invoerveld
     console.log(inputValue);
-    chatScreen.classList.add('focus')
+    chatScreen.classList.add('focus');
     socket.emit('focus', true); // Verzend de focus class naar andere clients
-
 });
 
 
 sendMessage.addEventListener('click', (event) => {
     chatScreen.classList.remove('focus')
     socket.emit('focus', false); // Verzend de focus class naar andere clients
-// 
 
     event.preventDefault();
     if (input.value) {
@@ -61,6 +113,3 @@ socket.on('focus', (hasFocus) => {
         chatScreen.classList.remove('focus');
     }
 });
-
-
-
