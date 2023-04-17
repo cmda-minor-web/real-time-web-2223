17fd9263c50ee75a68748d80560ddda71d4144bf
SundousKanaan <94317411+SundousKanaan@users.noreply.github.com>
2023-04-17 23:19:04 +0200

    test 4

--- public/script/script.js
+++ public/script/script.js
@@ -1,58 +1,5 @@
-// const messages = document.querySelector('section ul');
-// const input = document.querySelector('#message-input');
-// const sendMessage = document.querySelector('#message-button');
-// const usernameInput = document.querySelector('#username-input');
-// const loggin= document.querySelector('main section:first-of-type')
-// const chatScreen= document.querySelector('main section:last-of-type')
-// const logginButton = document.querySelector('main section:first-of-type > button')
-
-// chatScreen.classList.add("hidden");
-
-// logginButton.addEventListener('click' , () => {
-//     loggin.classList.add("hidden");
-//     chatScreen.classList.remove("hidden");
-
-// })
-
-
-// input.addEventListener('input', () => {
-//     const inputValue = input.value;
-//     // Doe hier iets met de waarde van het invoerveld
-//     console.log(inputValue);
-//     chatScreen.classList.add('focus')
-// });
-
-
-// sendMessage.addEventListener('click', (event) => {
-//     chatScreen.classList.remove('focus')
-
-//     event.preventDefault();
-//     if (input.value) {
-
-//         const chat ={
-//             username: usernameInput.value,
-//             message: input.value
-//         }
-
-//         socket.emit('chat message', chat);
-//         input.value = '';
-//       }
-// });
-
-// socket.on('chat message', (msg) => {
-//     console.log('chat message: ', msg.message);
-//     console.log(chatScreen);
-
-//     const element = document.createElement('li');
-//     element.textContent = ` ${msg.username}: ${msg.message} `;
-
-//     messages.appendChild(element);
-//     messages.scrollTop = messages.scrollHeight;
-// });
-
-
-
 const messages = document.querySelector('section ul');
+
 const input = document.querySelector('#message-input');
 const sendMessage = document.querySelector('#message-button');
 const usernameInput = document.querySelector('#username-input');
@@ -92,7 +39,7 @@ sendMessage.addEventListener('click', (event) => {
 
         socket.emit('chat message', chat);
         input.value = '';
-      }
+    }
 });
 
 socket.on('chat message', (msg) => {
@@ -101,7 +48,7 @@ socket.on('chat message', (msg) => {
 
     const element = document.createElement('li');
     element.textContent = ` ${msg.username}: ${msg.message} `;
-
+    element.classList.add('message')
     messages.appendChild(element);
     messages.scrollTop = messages.scrollHeight;
 });
