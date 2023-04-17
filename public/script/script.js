255cc740fd45a0e98a5438a753167bbb011cf0ca
SundousKanaan <94317411+SundousKanaan@users.noreply.github.com>
2023-04-17 15:24:40 +0200

    new test

--- public/script/script.js
+++ public/script/script.js
@@ -2,16 +2,16 @@ const messages = document.querySelector('section ul');
 const input = document.querySelector('#message-input');
 const submit = document.querySelector('#message-button');
 const usernameInput = document.querySelector('#username-input');
-const usernameSubmit = document.querySelector("#username-button");
+// const usernameSubmit = document.querySelector("#username-button");
 const loggin= document.querySelector('main section:first-of-type')
 var userNames=[];
 
-usernameSubmit.addEventListener('click', (event) => {
-    event.preventDefault(); // voorkomt de standaardgedrag van het formulier
-    userNames.push(usernameInput.value)
-    console.log(userNames);
-    loggin.classList.add('hidden')
-})
+// usernameSubmit.addEventListener('click', (event) => {
+//     event.preventDefault(); // voorkomt de standaardgedrag van het formulier
+//     userNames.push(usernameInput.value)
+//     console.log(userNames);
+//     // loggin.classList.add('hidden')
+// })
 
 submit.addEventListener('click', (event) => {
     event.preventDefault();
@@ -23,12 +23,12 @@ submit.addEventListener('click', (event) => {
 
 socket.on('chat message', (msg) => {
     console.log('chat message: ' + msg);
-    const currentUserName = userNames[userNames.length - 1]; // krijg de meest recente gebruikersnaam
+    // const currentUserName = userNames[userNames.length - 1]; // krijg de meest recente gebruikersnaam
 
     const element = document.createElement('li');
     const userName = document.createElement('p')
     element.textContent = msg;
-    userName.textContent = currentUserName;
+    userName.textContent = usernameInput.value;
 
     element.appendChild(userName);
     messages.appendChild(element);
