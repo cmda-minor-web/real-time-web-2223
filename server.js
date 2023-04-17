973233d24341337564cb3c3e086225615ab9d3a0
SundousKanaan <94317411+SundousKanaan@users.noreply.github.com>
2023-04-17 13:07:14 +0200

    socket workt

--- server.js
+++ server.js
@@ -1,11 +1,50 @@
 const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+const port = process.env.PORT || 4242
+
+
+
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+app.use(express.static("public"))
+
+
+// home page
+app.get('/', async (req, res) => {
+    try {
+        res.render('index');
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+})
+
+
+// We passen het server script aan om een console bericht te loggen zodra 
+// er een gebruiker verbinding maakt met via socket.io, dat zie je aan het connection event.
+io.on('connection', (socket) => {
+    console.log('connected');
+
+    socket.on('chat message', (msg) => {
+        console.log('chat message: ' + msg);
+        io.emit('chat message', msg); // broadcast the message to all clients
+      });
+
+    // Als een gebruiker connectie maakt zie je de log message die we ingesteld hebben, 
+    // misschien willen we ook zien wanneer een gebruiker disconnect.
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+});
+
+
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hallo wereld! LOL</h1>')
+    //   response.send('<h1>Hallo wereld! LOL</h1>')
+    response.render('index')
 })
 
 http.listen(4242, () => {
-  console.log('listening on 4242')
+    console.log('listening on 4242')
 })
