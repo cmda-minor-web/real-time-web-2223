68a117df217b33f45318279c5384760ebd552ad7
SundousKanaan <94317411+SundousKanaan@users.noreply.github.com>
2023-04-17 14:43:31 +0200

    user name workt

--- public/script/script.js
+++ public/script/script.js
@@ -1,20 +1,39 @@
 const messages = document.querySelector('section ul');
-const input = document.querySelector('input[type=text]');
-const submit = document.querySelector('button[type=submit]');
+const input = document.querySelector('#message-input');
+const submit = document.querySelector('#message-button');
+const usernameInput = document.querySelector('#username-input');
+const usernameSubmit = document.querySelector("#username-button");
+var userName;
+
+usernameSubmit.addEventListener('click', (event) => {
+    event.preventDefault(); // voorkomt de standaardgedrag van het formulier
+    userName = usernameInput.value;
+    console.log(userName);
+})
+
+
+console.log("userName", userName);
+
 
 submit.addEventListener('click', (event) => {
     event.preventDefault();
     if (input.value) {
         socket.emit('chat message', input.value);
         input.value = '';
-    }
+      }
 });
 
 socket.on('chat message', (msg) => {
+    console.log("userName", userName);
     console.log('chat message: ' + msg);
     const element = document.createElement('li');
+    const name = document.createElement('p')
     element.textContent = msg;
+    name.textContent = userName;
+    element.appendChild(name);
     messages.appendChild(element);
     messages.scrollTop = messages.scrollHeight;
 });
+
+
 
--- server.js
+++ server.js
@@ -1,13 +1,63 @@
-const express = require('express')
-const app = express()
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
-const port = process.env.PORT || 4242
+// const express = require('express')
+// const app = express()
+// const http = require('http').createServer(app)
+// const io = require('socket.io')(http)
+// const port = process.env.PORT || 4242
+
+// app.set('views', 'views');
+// app.set('view engine', 'ejs');
+// app.use(express.static("public"))
+
+
+// // home page
+// app.get('/', async (req, res) => {
+//     try {
+//         res.render('index');
+//     } catch (error) {
+//         res.status(500).send(error.message);
+//     }
+// })
+
+
+// // We passen het server script aan om een console bericht te loggen zodra 
+// // er een gebruiker verbinding maakt met via socket.io, dat zie je aan het connection event.
+// io.on('connection', (socket) => {
+//     console.log('connected');
+
+//     socket.on('chat message', (username, message) => {
+//         console.log(`${username}: ${message}`);
+//         io.emit('chat message', username, message); // broadcast the message to all clients
+//       });
+
+//     // Als een gebruiker connectie maakt zie je de log message die we ingesteld hebben, 
+//     // misschien willen we ook zien wanneer een gebruiker disconnect.
+//     socket.on('disconnect', () => {
+//         console.log('user disconnected')
+//     })
+// });
+
+
+
+// app.get('/', (request, response) => {
+//     //   response.send('<h1>Hallo wereld! LOL</h1>')
+//     response.render('index')
+// })
+
+// http.listen(port, () => {
+//     console.log('listening on port:', port)
+// })
+
+// // ***********************************************************
+
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 4242;
 
 app.set('views', 'views');
 app.set('view engine', 'ejs');
-app.use(express.static("public"))
-
+app.use(express.static("public"));
 
 // home page
 app.get('/', async (req, res) => {
@@ -16,33 +66,22 @@ app.get('/', async (req, res) => {
     } catch (error) {
         res.status(500).send(error.message);
     }
-})
-
-
-// We passen het server script aan om een console bericht te loggen zodra 
-// er een gebruiker verbinding maakt met via socket.io, dat zie je aan het connection event.
-io.on('connection', (socket) => {
-    console.log('connected');
-
-    socket.on('chat message', (msg) => {
-        console.log('chat message: ' + msg);
-        io.emit('chat message', msg); // broadcast the message to all clients
-      });
-
-    // Als een gebruiker connectie maakt zie je de log message die we ingesteld hebben, 
-    // misschien willen we ook zien wanneer een gebruiker disconnect.
-    socket.on('disconnect', () => {
-        console.log('user disconnected')
-    })
 });
 
+io.on('connection', (socket) => {
+    console.log('A user connected.');
 
+    socket.on('chat message', (data) => {
+        const [username, message] = data.split(': ');
+        console.log(`${username}: ${message}`);
+        io.emit('chat message', data);
+    });
 
-app.get('/', (request, response) => {
-    //   response.send('<h1>Hallo wereld! LOL</h1>')
-    response.render('index')
-})
+    socket.on('disconnect', () => {
+        console.log('A user disconnected.');
+    });
+});
 
 http.listen(port, () => {
-    console.log('listening on port:', port)
-})
+    console.log(`Listening on port: ${port}`);
+});
