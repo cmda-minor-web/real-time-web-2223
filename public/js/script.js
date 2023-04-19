661f9a8e9cde7a00d7c7eda3ea5bc38dd83fc925
Inevdhoven <inevdhoven@gmail.com>
2023-04-19 08:30:09 +0200

    Added code team assignment and feature history

---
+++ public/js/script.js
@@ -0,0 +1,52 @@
+let socket = io();
+let messages = document.querySelector('section ul');
+let inputText = document.querySelector('input#message');
+let inputName = document.querySelector('input#name');
+let send = document.querySelector('button#send');
+let typingState = document.querySelector('p');
+
+// send text
+document.querySelector('form').addEventListener('submit', event => {
+  event.preventDefault()
+  let data = { name: inputName.value, message: inputText.value }
+  socket.emit('chat', data);
+  console.log(inputName.value, inputText.value);
+  inputText.value = '';
+})
+
+inputText.addEventListener('keypress', () => {
+  socket.emit('typing', inputName.value)
+})
+
+socket.on('history', (history) => {
+  history.forEach((data) => {
+    addMessage(data)
+  })
+})
+
+socket.on('chat', (data) => {
+  let li = document.createElement('li');
+  li.textContent = data.name + ': ' + data.message;
+
+  // Check if the message is sent by the user
+  if (data.name === inputName.value) {
+    li.classList.add('current-user');
+  }
+
+  messages.appendChild(li);
+  typingState.innerHTML = "";
+  messages.scrollTop = messages.scrollHeight
+})
+
+socket.on('typing', (inputName) => {
+  console.log(inputName);
+  typingState.innerHTML = (inputName + " is aan het typen...")
+  setTimeout(() => {
+    typingState.innerHTML = "";
+  }, 3000);
+})
+
+function addMessage(data) {
+  messages.appendChild(Object.assign(document.createElement('li'), { textContent: data.name + ': ' + data.message }))
+  messages.scrollTop = messages.scrollHeight
+}
---
+++ server.js
@@ -0,0 +1,46 @@
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+import http from 'http';
+import { Server } from 'socket.io';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 4300;
+const server = http.createServer(app);
+const io = new Server(server);
+const historySize = 50
+let history = []
+
+app.use(express.static(path.resolve('public')));
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.emit('history', history)
+
+    socket.on('chat', (data) => {
+        console.log(data);
+        while (history.length > historySize) {
+            history.shift()
+        }
+        history.push(data)
+        io.sockets.emit("chat", data);
+    });
+
+    socket.on('typing', (inputName) => {
+        console.log("Aan het typen");
+        socket.broadcast.emit("typing", inputName);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+});
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
+// AIzaSyAYU-OHa37BuQfM1nJ83Ps-Haj-HrGVr7A
